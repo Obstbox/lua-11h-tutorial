e0aab5a4ea7f7f2bff93bb89dd2328b2a39bf25c
ivan pashchenko <obst.box@gmail.com>
2024-04-04 20:45:56 +0500

    3:21:00 so flip him

--- main.lua
+++ main.lua
@@ -1,21 +1,6 @@
 _G.love = require("love")
 
 function love.load()
-    -- love.graphics.setBackgroundColor(0.5, 0.5, 1)
-
-    -- -- explicit define pacman as a global
-    -- -- _G.pacman = {}
-    -- pacman = {}
-    -- pacman.x = 200
-    -- pacman.y = 250
-    -- pacman.size = 60
-    -- pacman.angle1 = 0.5
-    -- pacman.angle2 = 5.5
-
-    -- _G.food = {
-    --     x = 400,
-    --     eaten = false
-    -- }
     jack = {
         x = 0,
         y = 0,
@@ -25,7 +10,7 @@ function love.load()
             idle = true,
             frame = 1,
             max_frames = 4,
-            speed = 10,
+            speed = 16,
             timer = 0.1
         }
     }
@@ -58,43 +43,8 @@ function love.load()
 end
 
 function love.update(dt)
-    -- delta = 2
 
-    -- if love.keyboard.isDown("down") then
-    --     pacman.angle1 = pacman.angle1 + math.pi * dt * 2
-    --     pacman.angle2 = pacman.angle2 + math.pi * dt * 2
-    -- end
-
-    -- if love.keyboard.isDown("up") then
-    --     pacman.angle1 = pacman.angle1 - math.pi * dt * 2
-    --     pacman.angle2 = pacman.angle2 - math.pi * dt * 2
-    -- end
-
-    -- if love.keyboard.isDown("a") then
-    --     if pacman.x >= pacman.size then
-    --         pacman.x = pacman.x - delta
-    --     end
-    -- end
-    -- if love.keyboard.isDown("d") then
-    --     -- how to access global window width?
-    --     if pacman.x <= 680 - pacman.size then
-    --         pacman.x = pacman.x + delta
-    --     end
-    -- end
-    -- if love.keyboard.isDown("w") then
-    --     if pacman.y >= pacman.size then
-    --         pacman.y = pacman.y - delta
-    --     end
-    -- end
-    -- if love.keyboard.isDown("s") then
-    --     if pacman.y <= 480 - pacman.size then
-    --         pacman.y = pacman.y + delta
-    --     end
-    -- end
-
-    -- if pacman.x >= food.x + 20 then
-    --     food.eaten = true
-    -- end
+    -- how to access global window width?
 
     if love.keyboard.isDown("a") then
         jack.animation.idle = false
@@ -144,14 +94,13 @@ function love.update(dt)
 end
 
 function love.draw()
-    -- if not food.eaten then
-    --     love.graphics.setColor(62/255, 62/255, 62/255)
-    --     love.graphics.rectangle("fill", food.x, 225, 50, 50)
-    -- end
-
-    -- love.graphics.setColor(1, 0.7, 0.1)
-    -- love.graphics.arc("fill", pacman.x, pacman.y, pacman.size, pacman.angle1, pacman.angle2)
-    love.graphics.scale(2)
-    love.graphics.draw(jack.sprite, quads_right[jack.animation.frame], jack.x, jack.y)
+    -- love.graphics.scale(2)
+    if jack.animation.direction == "right" then
+        love.graphics.draw(jack.sprite, quads_right[jack.animation.frame], jack.x, jack.y)
+    else
+        -- flip by x-axis ( .., -1, .. )
+        love.graphics.draw(jack.sprite, quads_right[jack.animation.frame], jack.x, jack.y,
+        0, -1, 1, QUAD_WIDTH, 0)
+    end
 end
 
