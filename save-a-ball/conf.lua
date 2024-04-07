38158214f14e180398ebcb8fca95251ad2e2959b
ivan pashchenko <obst.box@gmail.com>
2024-04-07 18:39:42 +0500

    3:39:10 set the ball and the FPS counter. now implement an enemy

---
+++ save-a-ball/conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+    t.version = "11.4"
+    t.window.width = 680
+    t.window.height = 480
+end
---
+++ save-a-ball/main.lua
@@ -0,0 +1,44 @@
+local love = require("love")
+
+local game = {
+    state = {
+        menu = true,
+        paused = false,
+        running = false,
+        ended = false,
+    }
+
+}
+
+local player = {
+    radius = 20,
+    x = 30,
+    y = 30
+}
+
+function love.load()
+    love.window.title = "Save the ball"
+    love.mouse.setVisible(false)
+end
+
+function love.update(dt)
+    player.x, player.y = love.mouse.getPosition()
+end
+
+function love.draw()
+    love.graphics.printf(
+        "FPS: " .. love.timer.getFPS(),
+        love.graphics.newFont(12),
+        10,
+        love.graphics.getHeight() - 30,
+        love.graphics.getWidth()
+    )
+
+    if game.state['running'] then
+        love.graphics.circle("fill", player.x, player.y, player.radius)
+    end
+
+    if not game.state['running'] then
+        love.graphics.circle("fill", player.x, player.y, player.radius / 2)
+    end
+end
