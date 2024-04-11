f366b99d216e0e5c044adfb6ad2860888fe899ce
ivan pashchenko <obst.box@gmail.com>
2024-04-11 13:42:23 +0500

    5:01:38> got some of state machine

--- save-a-ball/main.lua
+++ save-a-ball/main.lua
@@ -29,9 +29,15 @@ local buttons = {
 
 local enemies = {}
 
+local function changeGameState(state)
+    game.state["menu"]    = state == "menu"     -- shortened if-else statement
+    game.state["paused"]  = state == "paused"
+    game.state["running"] = state == "running"
+    game.state["ended"]   = state == "ended"
+end
+
 local function startNewGame()
-    game.state["menu"] = false
-    game.state["running"] = true
+    changeGameState("running")
 
     game.points = 0
 
