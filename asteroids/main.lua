b0b3a49ba4cf7a895fb1d3b74ac348401566557b
ivan pashchenko <obst.box@gmail.com>
2024-04-12 23:31:40 +0500

    5:49:24> a Player file

--- asteroids/main.lua
+++ asteroids/main.lua
@@ -1,12 +1,15 @@
 local love = require "love"
 
 function love.load()
+    love.mouse.setVisible(false)
+    mouse_x, mouse_y = 0, 0
 
+    local show_debugging = true
 end
 
 
 function love.update()
-
+    mouse_x, mouse_y = love.mouse.getPosition()
 end
 
 
